f5827a51fee31f6b201d82c97ded8304ee787374
Vitor Nascimento <vitor.gan21@gmail.com>
2021-02-21 20:03:00 -0300

    Adicionado return de personagem

--- src/api/controllers/personagens.js
+++ src/api/controllers/personagens.js
@@ -1,6 +1,15 @@
+const { Personagem } = require('../../models');
+
 module.exports = {
   getPersonagem: (req, res) => {
-    const personagem = { nome: `Merrik d'Milliard`, jogador: 'V' };
+    const personagemBase = {
+      nome: `Merrik d'Milliard`,
+      jogador: 'V',
+      nacionalidade: 'Canadense',
+      ocupacao: 'instrutor de esgrima',
+      residencia: 'Estados Unidos',
+    };
+    const personagem = new Personagem(personagemBase);
 
     res.send({ sucesso: true, personagem });
   },
