42fef4d49bb39b9e536b1b5da18fcd5357f09d12
Vitor Nascimento <vitor.gan21@gmail.com>
2021-03-06 14:23:01 -0300

    Adicionado controller para personagem

--- src/api/controllers/personagens.js
+++ src/api/controllers/personagens.js
@@ -1,5 +1,22 @@
 const { Personagem } = require('../../models');
 
+class PersonagemController {
+  static getPersonagem(req, res) {
+    const personagemBase = {
+      nome: `Merrik d'Milliard`,
+      jogador: 'V',
+      nacionalidade: 'Canadense',
+      ocupacao: 'instrutor de esgrima',
+      residencia: 'Estados Unidos',
+    };
+    const personagem = new Personagem(personagemBase);
+
+    res.send({ sucesso: true, personagem });
+  }
+}
+
+module.exports = PersonagemController;
+
 module.exports = {
   getPersonagem: (req, res) => {
     const personagemBase = {
