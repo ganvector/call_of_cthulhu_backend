408f27b1af635551d379270f1794e4df4a6a8937
Vitor Nascimento <vitor.gan21@gmail.com>
2021-03-06 14:35:50 -0300

    Retirada função de teste

--- src/api/controllers/personagens.js
+++ src/api/controllers/personagens.js
@@ -16,18 +16,3 @@ class PersonagemController {
 }
 
 module.exports = PersonagemController;
-
-module.exports = {
-  getPersonagem: (req, res) => {
-    const personagemBase = {
-      nome: `Merrik d'Milliard`,
-      jogador: 'V',
-      nacionalidade: 'Canadense',
-      ocupacao: 'instrutor de esgrima',
-      residencia: 'Estados Unidos',
-    };
-    const personagem = new Personagem(personagemBase);
-
-    res.send({ sucesso: true, personagem });
-  },
-};
