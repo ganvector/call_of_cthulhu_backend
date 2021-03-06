649d357be6269c4445217ccee794227c31ba04f1
Vitor Nascimento <vitor.gan21@gmail.com>
2021-03-06 14:21:58 -0300

    index movido para pasta api

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
 const { getPersonagem } = require('../controllers/personagens');
+const personagemRoutes = require('./personagem');
 
 module.exports.initRoutes = (app) => {
+  app.get('/personagem', personagemRoutes);
   app.get('/getPersonagem', getPersonagem);
 };
