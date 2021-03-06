6678020f4e92a27bec25960f28df4efd589573fe
Vitor Nascimento <vitor.gan21@gmail.com>
2021-03-06 14:25:36 -0300

    Refeito rota para rota de personagens

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,3 @@
-const { getPersonagem } = require('../controllers/personagens');
-const personagemRoutes = require('./personagem');
-
 module.exports.initRoutes = (app) => {
-  app.get('/personagem', personagemRoutes);
-  app.get('/getPersonagem', getPersonagem);
+  app.use('/api/personagem', require('./routes/personagem'));
 };
